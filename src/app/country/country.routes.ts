9e427096e98f060b715b903378d77b11e276a948
wfgp12 <w.gutierrez1206@gmail.com>
2025-04-25 23:03:44 -0500

    feat: configured routes for country section
    - Added and set up routes related to displaying and managing countries.
    - Prepared views and associated components for these routes.

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,15 +1,26 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
 
 export const countryRoutes: Routes = [
     {
         path: '',
-        component: ByCapitalPageComponent
+        component: CountryLayoutComponent,
+        children: [
+            {
+                path: 'by-capital',
+                component: ByCapitalPageComponent
+            },
+            {
+                path: '**',
+                redirectTo: 'by-capital',
+            }
+        ]
     },
-    {
-        path: '**',
-        redirectTo: '',
-    }
+    // {
+    //     path: '**',
+    //     redirectTo: '',
+    // }
 ];
 
 export default countryRoutes;
---
+++ src/app/country/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [RouterOutlet],
+  templateUrl: './country-layout.component.html',
+})
+export class CountryLayoutComponent { }
